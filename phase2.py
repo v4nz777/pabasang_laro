044f4dc8b6c631b0cd8c3e5a92bf3abcb33f96ca
Van Henry Salido <vansalido@gmail.com>
2022-07-26 12:02:56 +0800

    ForDroid

---
+++ phase2.py
@@ -0,0 +1,192 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.core.audio import SoundLoader
+from kivy.uix.carousel import Carousel
+from kivy.uix.scatter import Scatter
+from kivy.animation import Animation
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty, ReferenceListProperty, NumericProperty, StringProperty, ListProperty
+
+
+#from sound_recorder import rec
+from jniusrecord import android_record, play_audio
+from kivy.utils import platform
+
+
+
+import random
+
+from materials import FILIPINO_ALPHABETS, PHONOGRAM_ALPHA, PHONOGRAM_MULTI_CT, PHONOGRAM_MULTI_VT
+
+from materials import lowercase as fil_lowercase
+
+
+class PhonogramAlpha(MDScreen):
+    toolbar = ObjectProperty(None)
+    carousel = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(PhonogramAlpha, self).__init__(**kwargs)
+        self.phonograms_alpha = self.ids.phonograms_alpha
+        self.phonograms_ct = self.ids.phonograms_ct
+        self.phonograms_vt = self.ids.phonograms_vt
+
+        self.sound = None
+        self.button = None
+
+        for letter in PHONOGRAM_ALPHA:
+            self.button = MDRectangleFlatButton(text=letter,font_size='30sp', size_hint_y=None, font_name='NotoSerif-Regular.ttf')
+            self.button.bind(on_press=self.letter_sound)
+            self.phonograms_alpha.add_widget(self.button)
+        
+        for letter in PHONOGRAM_MULTI_CT:
+            self.button = MDRectangleFlatButton(text=letter,font_size='25sp', size_hint_y=None, font_name='NotoSerif-Regular.ttf')
+            self.button.bind(on_press=self.letter_sound)
+            self.phonograms_ct.add_widget(self.button)
+        
+        for letter in PHONOGRAM_MULTI_VT:
+            self.button = MDRectangleFlatButton(text=letter,font_size='25sp', size_hint_y=None, font_name='NotoSerif-Regular.ttf')
+            self.button.bind(on_press=self.letter_sound)
+            self.phonograms_vt.add_widget(self.button)
+
+    def letter_sound(self,instance):
+        if instance.text:
+            if self.sound:
+                self.sound.stop() 
+            filename = instance.text
+            self.sound = SoundLoader.load(f'src/phase2/phonogram/{filename}.ogg')
+            self.sound.play()
+
+class Carou(Carousel):
+    def on_touch_move(self, touch):
+        if self.index == 1:
+            self.parent.toolbar.disabled = False
+        return super().on_touch_move(touch)
+
+
+class PhaseTwoTest(MDScreen):
+    the_letter = ObjectProperty(None)
+    progress_bar = ObjectProperty(None)
+    microphone = ObjectProperty(None)
+    speaker = ObjectProperty(None)
+    next_button = ObjectProperty(None)
+    toolbar = ObjectProperty(None)
+    sound = ObjectProperty(None)
+
+    tries = ListProperty([])
+    corrected = ListProperty([])
+    current_answer = StringProperty("")
+    answer_file = StringProperty("")
+
+    def __init__(self, **kw):
+        super(PhaseTwoTest, self).__init__(**kw)
+        self.the_letter.final_pos = self.the_letter.pos
+    
+    def show_letter(self):
+        zzz = len(self.corrected)/28
+        self.progress_bar.value = zzz * 100
+        if len(self.corrected) != 28:
+            self.current_answer = random.choice(fil_lowercase)
+            while self.current_answer in self.corrected:
+                self.current_answer = random.choice(fil_lowercase)
+
+            self.the_letter.final_pos = self.the_letter.pos
+            x,y = self.the_letter.final_pos
+            self.the_letter.pos = (x, y+self.height)
+
+            self.the_letter.children[0].text = f"{self.current_answer}"
+            anim1 = Animation(y=y, t='out_bounce')
+            anim1.start(self.the_letter)
+            self.corrected.append(self.current_answer)
+            print(self.current_answer)
+        else:
+            self.toolbar.disabled = False
+            print('ALL Letters ALREADY ANSWERED')
+        self.speaker.theme_icon_color = "Primary"
+        self.next_button.disabled = True
+
+    def start_show_letter(self, dt):
+
+        if len(self.corrected) != 28:
+            self.current_answer = random.choice(fil_lowercase)
+            while self.current_answer in self.corrected:
+                self.current_answer = random.choice(fil_lowercase)
+
+            self.the_letter.final_pos = self.the_letter.pos
+            x,y = self.the_letter.final_pos
+            self.the_letter.pos = (x, y+self.height)
+
+            self.the_letter.children[0].text = f"{self.current_answer}"
+            anim1 = Animation(y=y, t='out_bounce')
+            anim1.start(self.the_letter)
+            self.corrected.append(self.current_answer)
+            print(self.current_answer)
+        else:
+            self.toolbar.disabled = False
+            print('ALL Letters ALREADY ANSWERED')
+        self.speaker.theme_icon_color = "Primary"
+        self.next_button.disabled = True
+
+    def on_enter(self):
+        Clock.schedule_once(self.start_show_letter)
+
+    def validate(self):
+        self.sound = SoundLoader.load('src/pre_record_sound.ogg')
+        self.sound.play()
+        answer = self.answer()
+        while not answer:
+            #self.microphone = MDSpinner()
+            self.microphone.disabled = True
+            self.microphone.theme_icon_color="Custom"
+            self.microphone.icon = "microphone-question"
+            self.microphone.icon_color = (0,1,0.3,1)
+          
+        
+        if self.answer_file in self.tries:
+            self.speaker.theme_icon_color="Custom"
+            self.speaker.icon_color = (0,1,0.3,1)
+            self.next_button.disabled = False
+
+        #then autoplay recorded
+        self.listen_answer()
+        
+
+
+    def answer(self):
+        self.answer_file = self.current_answer + '.m4a'
+        duration = 4
+        freq = 44100
+        #record = rec(f'answers/phase2/{self.answer_file}')
+        record = android_record(f'/storage/emulated/0/pb/answers/phase2/{self.answer_file}')
+
+        self.tries.append(self.answer_file)
+        return record
+    
+
+
+    
+    def listen_answer(self):
+        self.answer_file = self.current_answer + '.m4a'
+
+        if self.answer_file in self.tries:
+
+            path_to_file = f'answers/phase2/{self.answer_file}'
+            
+            if platform == "android":
+                path_to_file = f'/storage/emulated/0/pb/answers/phase2/{self.answer_file}'
+                play_audio(path_to_file)
+            else:
+                if self.sound:
+                    self.sound.stop()
+                self.sound = SoundLoader.load(path_to_file)
+                self.sound.play()
+        else:
+            print('answer first!')
+
+
+
+class TheLetter(Scatter):
+    xxx = NumericProperty(0)
+    yyy = NumericProperty(0)
+    initial_pos = ReferenceListProperty(xxx,yyy)
+    final_pos = ReferenceListProperty(xxx,yyy)
---
+++ phase4.py
@@ -0,0 +1,368 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFillRoundFlatButton
+from kivy.core.audio import SoundLoader
+from kivy.uix.scatter import Scatter
+from kivy.animation import Animation
+from kivy.metrics import dp
+from kivy.properties import ObjectProperty, ReferenceListProperty, NumericProperty, StringProperty, ListProperty
+from materials import CVCE,CVCA, CVCE, CVCI, CVCO, CVCU, CVC_QUIZ, CVCLONGA, CVCLONGE, CVCLONGI, CVCLONGO, CVCLONGU
+import random
+from kivy.clock import Clock
+
+#from sound_recorder import rec
+from jniusrecord import android_record, play_audio
+from kivy.utils import platform
+
+
+class Cvc(MDScreen):
+    pass
+
+class VowelA(MDScreen):
+    def __init__(self, **kw):
+        super(VowelA, self).__init__(**kw)
+        self._list = self.ids.cvc_a
+        self.say_word = None
+        for i in CVCA:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/short/a/{instance.text}.ogg')
+            self.say_word.play()
+    
+class VowelE(MDScreen):
+    def __init__(self, **kw):
+        super(VowelE, self).__init__(**kw)
+        self._list = self.ids.cvc_e
+        self.say_word = None
+        for i in CVCE:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/short/e/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelI(MDScreen):
+    def __init__(self, **kw):
+        super(VowelI, self).__init__(**kw)
+        self._list = self.ids.cvc_i
+        self.say_word = None
+        for i in CVCI:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/short/i/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelO(MDScreen):
+    def __init__(self, **kw):
+        super(VowelO, self).__init__(**kw)
+        self._list = self.ids.cvc_o
+        self.say_word = None
+        for i in CVCO:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/short/o/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelU(MDScreen):
+    def __init__(self, **kw):
+        super(VowelU, self).__init__(**kw)
+        self._list = self.ids.cvc_u
+        self.say_word = None
+        for i in CVCU:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/short/u/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelALong(MDScreen):
+    def __init__(self, **kw):
+        super(VowelALong, self).__init__(**kw)
+        self._list = self.ids.cvc_a
+        self.say_word = None
+        for i in CVCLONGA:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/long/a/{instance.text}.ogg')
+            self.say_word.play()
+    
+class VowelELong(MDScreen):
+    def __init__(self, **kw):
+        super(VowelELong, self).__init__(**kw)
+        self._list = self.ids.cvc_e
+        self.say_word = None
+        for i in CVCLONGE:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/long/e/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelILong(MDScreen):
+    def __init__(self, **kw):
+        super(VowelILong, self).__init__(**kw)
+        self._list = self.ids.cvc_i
+        self.say_word = None
+        for i in CVCLONGI:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/long/i/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelOLong(MDScreen):
+    def __init__(self, **kw):
+        super(VowelOLong, self).__init__(**kw)
+        self._list = self.ids.cvc_o
+        self.say_word = None
+        for i in CVCLONGO:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/long/o/{instance.text}.ogg')
+            self.say_word.play()
+
+class VowelULong(MDScreen):
+    def __init__(self, **kw):
+        super(VowelULong, self).__init__(**kw)
+        self._list = self.ids.cvc_u
+        self.say_word = None
+        for i in CVCLONGU:
+            if True: #if len(i) == 3:
+                self.btn = MDFillRoundFlatButton(
+                    text=i, 
+                    font_size=dp(100), 
+                    
+                    pos_hint={"center_x": .5, "center_y": .5},
+                )
+                self.btn.bind(on_release=self.callbck)
+                self._list.add_widget(self.btn)
+            
+    def callbck(self,instance):
+        if instance.text:
+            if self.say_word:
+                self.say_word.stop()
+            self.say_word = SoundLoader.load(f'src/phase4/long/u/{instance.text}.ogg')
+            self.say_word.play()
+###################################### CVC GAME ############################################
+class CvcQuiz(MDScreen):
+    word_box = ObjectProperty(None)
+    shown = ListProperty([])
+    current_answer = StringProperty("")
+    tries = ListProperty([])
+
+    sound = ObjectProperty(None)
+    
+    progress_bar = ObjectProperty(None)
+    microphone = ObjectProperty(None)
+    speaker = ObjectProperty(None)
+    next_button = ObjectProperty(None)
+    toolbar = ObjectProperty(None)
+
+    def show_word(self):
+        zzz = len(self.shown)/50
+        self.progress_bar.value = zzz * 100
+        if len(self.shown) != 50:
+            self.current_answer = random.choice(CVC_QUIZ)
+            while self.current_answer in self.shown:
+                self.current_answer = random.choice(CVC_QUIZ)
+
+            self.word_box.final_pos = self.word_box.pos
+            x,y = self.word_box.final_pos
+            self.word_box.pos = (x, y+self.height)
+
+            self.word_box.children[0].source = f"imgs/word_blocks/{self.current_answer}.png"
+            anim1 = Animation(y=y, size=(80, 80), t='out_bounce')
+            anim1.start(self.word_box)
+            self.shown.append(self.current_answer)
+            print(self.current_answer)
+        else:
+            self.toolbar.disabled = False
+            print('ALL 50 ALREADY ANSWERED')
+
+        self.speaker.theme_icon_color = "Primary"
+        self.next_button.disabled = True
+
+    def start_show_word(self, dt):
+        if len(self.shown) != 50:
+            self.current_answer = random.choice(CVC_QUIZ)
+            while self.current_answer in self.shown:
+                self.current_answer = random.choice(CVC_QUIZ)
+            
+            self.word_box.final_pos = self.word_box.pos
+            x,y = self.word_box.final_pos
+            self.word_box.pos = (x, y+self.height)
+
+            self.word_box.children[0].source = f"imgs/word_blocks/{self.current_answer}.png"
+            anim1 = Animation(y=y, size=(80, 80), t='out_bounce')
+            anim1.start(self.word_box)
+            self.shown.append(self.current_answer)
+            print(self.current_answer)
+        else:
+            self.toolbar.disabled = False
+            print('ALL 50 ALREADY ANSWERED')
+        self.next_button.disabled = True
+
+    def on_enter(self):
+        Clock.schedule_once(self.start_show_word)
+
+    def validate(self):
+        self.sound = SoundLoader.load('src/pre_record_sound.ogg')
+        self.sound.play()
+        answer = self.answer()
+        while not answer:
+            #self.microphone = MDSpinner()
+            self.microphone.disabled = True
+            self.microphone.theme_icon_color="Custom"
+            self.microphone.icon = "microphone-question"
+            self.microphone.icon_color = (0,1,0.3,1)
+          
+        
+        if self.answer_file in self.tries:
+            self.speaker.theme_icon_color="Custom"
+            self.speaker.icon_color = (0,1,0.3,1)
+            self.next_button.disabled = False
+
+        #then autoplay recorded
+        self.listen_answer()
+        
+
+    def answer(self):
+        self.answer_file = self.current_answer + '.m4a'
+        duration = 4
+        freq = 44100
+        #record = rec(f'answers/phase4/{self.answer_file}')
+        record = android_record(f'/storage/emulated/0/pb/answers/phase4/{self.answer_file}')
+
+        self.tries.append(self.answer_file)
+        return record
+
+
+    def listen_answer(self):
+        self.answer_file = self.current_answer + '.m4a'
+
+        if self.answer_file in self.tries:
+            path_to_file = f'answers/phase4/{self.answer_file}'
+            
+            if platform == "android":
+                path_to_file = f'/storage/emulated/0/pb/answers/phase4/{self.answer_file}'
+                play_audio(path_to_file)
+            else:
+                if self.sound:
+                    self.sound.stop()
+                self.sound = SoundLoader.load(path_to_file)
+                self.sound.play()
+        else:
+            print('answer first!')
+         
+class WordBox(Scatter):
+    word = StringProperty("")
+    xxx = NumericProperty(0)
+    yyy = NumericProperty(0)
+    initial_pos = ReferenceListProperty(xxx,yyy)
+    final_pos = ReferenceListProperty(xxx,yyy)
+
+    
+    
